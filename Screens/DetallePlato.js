f3fba288318a1c2a85fe12e5dddf4c7c714a29a1
Cayo322 <112021884+Cayo322@users.noreply.github.com>
2023-12-10 19:49:41 -0500

    Renderizado

--- Screens/DetallePlato.js
+++ Screens/DetallePlato.js
@@ -28,7 +28,7 @@ const DetallePlato = () => {
                 const platosExcluyendoActual = filteredPlatos.filter(item => item.id !== plato.id);
                 const randomPlatos = platosExcluyendoActual.sort(() => 0.5 - Math.random()).slice(0, 3);
                 setPlatosMismoTipo(randomPlatos);
-                setComentarios(plato.comments);
+                setComentarios(plato.comments || []);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -58,92 +58,114 @@ const DetallePlato = () => {
                     rating: selectedRating
                 };
                 const response = await axios.post(`https://api-dishes-5r7a.onrender.com/api/platos/${plato.id}/rating`, ratingData);
-                // Actualizar el plato con la nueva calificación
-                setComentarios(response.data.comments);
-                setSelectedRating(0); // Reiniciar la calificación seleccionada
+                if (response.status === 200) {
+                    setComentarios(response.data.comments);
+                    setSelectedRating(0);
+                } else {
+                    console.log('Error en la respuesta del servidor:', response);
+                    Alert.alert('Error', 'Hubo un problema al calificar el plato. Por favor, inténtalo de nuevo.');
+                }
             } else {
-                // Muestra una alerta o maneja la situación donde no se ha seleccionado una calificación
+                // Resto del código...
             }
         } catch (error) {
             console.log(error);
+            Alert.alert('Error', 'Hubo un problema al calificar el plato. Por favor, inténtalo de nuevo.');
         }
     };
 
     return (
-        <ScrollView style={styles.container}>
-            <Image source={{ uri: plato.image.url }} style={styles.image} />
-            <View style={styles.platoDetailsContainer}>
-                <Text style={styles.platoName}>{plato.name}</Text>
-               
-                <View style={styles.ratingContainer}>
-                    <Text style={styles.ratingTitle}>Puntuación:</Text>
-                    <View style={styles.ratingStarsContainer}>
-                        {Array.from({ length: Math.floor(plato.rating) }).map((_, index) => (
-                            <Ionicons key={index} name="star" size={24} color="#FFD700" />
-                        ))}
-                        {Array.from({ length: Math.ceil(5 - plato.rating) }).map((_, index) => (
-                            <Ionicons key={index} name="star-outline" size={24} color="#FFD700" />
-                        ))}
-                    </View>
+      <ScrollView style={styles.container}>
+        <Image source={{ uri: plato.image.url }} style={styles.image} />
+        <View style={styles.platoDetailsContainer}>
+          <Text style={styles.platoName}>{plato.name}</Text>
+
+          <View style={styles.ratingContainer}>
+            <Text style={styles.ratingTitle}>Puntuación:</Text>
+            <View style={styles.ratingStarsContainer}>
+              {Array.from({ length: Math.floor(plato.rating) }).map(
+                (_, index) => (
+                  <Ionicons key={index} name="star" size={24} color="#FFD700" />
+                )
+              )}
+              {Array.from({ length: Math.ceil(5 - plato.rating) }).map(
+                (_, index) => (
+                  <Ionicons
+                    key={index}
+                    name="star-outline"
+                    size={24}
+                    color="#FFD700"
+                  />
+                )
+              )}
+            </View>
+          </View>
+
+          <Text style={styles.platoInfo}>Precio: ${plato.price}</Text>
+          <Text style={styles.platoInfo}>Descripción: {plato.description}</Text>
+          <Text style={styles.platoInfo}>Tipo: {plato.type}</Text>
+        </View>
+        <View style={styles.relatedPlatosContainer}>
+          <Text style={styles.relatedText}>Otros platos del mismo tipo:</Text>
+          <FlatList
+            data={platosMismoTipo}
+            keyExtractor={(item) => item.id}
+            horizontal
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => handlePlatoPress(item)}>
+                <View style={styles.relatedPlatoItem}>
+                  <Image
+                    source={{ uri: item.image.url }}
+                    style={styles.relatedPlatoImage}
+                  />
+                  <Text style={styles.relatedPlatoName}>{item.name}</Text>
                 </View>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+        <View style={styles.ratingContainer}>
+          <Text style={styles.ratingTitle}>Calificar el plato:</Text>
+          <View style={styles.ratingStarsContainer}>
+            {[1, 2, 3, 4, 5].map((star) => (
+              <TouchableOpacity
+                key={star}
+                style={[
+                  styles.star,
+                  star <= selectedRating && styles.selectedStar,
+                ]}
+                onPress={() => setSelectedRating(star)}
+              />
+            ))}
+          </View>
+          <TouchableOpacity
+            style={styles.submitRatingButton}
+            onPress={submitRating}
+          >
+            <Text style={styles.submitRatingButtonText}>Calificar</Text>
+          </TouchableOpacity>
+        </View>
 
-                <Text style={styles.platoInfo}>Precio: ${plato.price}</Text>
-                <Text style={styles.platoInfo}>Descripción: {plato.description}</Text>
-                <Text style={styles.platoInfo}>Tipo: {plato.type}</Text>
-            </View>
-            <View style={styles.relatedPlatosContainer}>
-                <Text style={styles.relatedText}>Otros platos del mismo tipo:</Text>
-                <FlatList
-                    data={platosMismoTipo}
-                    keyExtractor={(item) => item.id}
-                    horizontal
-                    renderItem={({ item }) => (
-                        <TouchableOpacity onPress={() => handlePlatoPress(item)}>
-                            <View style={styles.relatedPlatoItem}>
-                                <Image source={{ uri: item.image.url }} style={styles.relatedPlatoImage} />
-                                <Text style={styles.relatedPlatoName}>{item.name}</Text>
-                            </View>
-                        </TouchableOpacity>
-                    )}
-                />
-            </View>
-            <View style={styles.ratingContainer}>
-                <Text style={styles.ratingTitle}>Calificar el plato:</Text>
-                <View style={styles.ratingStarsContainer}>
-                    {[1, 2, 3, 4, 5].map((star) => (
-                        <TouchableOpacity
-                            key={star}
-                            style={[styles.star, star <= selectedRating && styles.selectedStar]}
-                            onPress={() => setSelectedRating(star)}
-                        />
-                    ))}
-                </View>
-                <TouchableOpacity style={styles.submitRatingButton} onPress={submitRating}>
-                    <Text style={styles.submitRatingButtonText}>Calificar</Text>
-                </TouchableOpacity>
-            </View>
+        <View style={styles.commentContainer}>
+            <Text style={styles.commentTitle}>Comentarios:</Text>
+            {comentarios && comentarios.map((comentario, index) => (
+                <Text key={index} style={styles.comment}>
+                    {comentario.comment}
+                </Text>
+            ))}
+        </View>
 
-         
 
-            <View style={styles.commentContainer}>
-                <Text style={styles.commentTitle}>Comentarios:</Text>
-                {comentarios.map((comentario, index) => (
-                    <Text key={index} style={styles.comment}>
-                        {comentario.comment}
-                    </Text>
-                ))}
-            </View>
-
-            <View style={styles.addCommentContainer}>
-                <TextInput
-                    style={styles.commentInput}
-                    placeholder="Agregar comentario..."
-                    onChangeText={(text) => setNuevoComentario(text)}
-                    value={nuevoComentario}
-                />
-                <Button title="Agregar Comentario" onPress={cometar} />
-            </View>
-        </ScrollView>
+        <View style={styles.addCommentContainer}>
+          <TextInput
+            style={styles.commentInput}
+            placeholder="Agregar comentario..."
+            onChangeText={(text) => setNuevoComentario(text)}
+            value={nuevoComentario}
+          />
+          <Button title="Agregar Comentario" onPress={cometar} />
+        </View>
+      </ScrollView>
     );
 };
 
